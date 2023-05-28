2c7399e51a2ddce469aba9248ef188e0f7f77de2
Piatol250 <sjw10@utas.edu.au>
2023-05-28 13:40:43 +1000

    Segregated files into respective folders

---
+++ RAP Program/Testing/PublicationTests.cs
@@ -0,0 +1,27 @@
+
+using System.Collections.Generic;
+
+namespace RAP_Program
+{
+    internal class PositionTests
+    {
+        public static void Collect(List<TestDriver.RunType> tests)
+        {
+            tests.Add(new TestDriver.RunType("load publications from an ID which doesn't exist", () =>
+                {
+                    List<Publication> all = PublicationController.loadPublications(-1);
+                    
+                    return (all.Count == 0);
+                }
+            ));
+
+            tests.Add(new TestDriver.RunType("load publications from an id which does exist", () =>
+                {
+                    List<Publication> all = PublicationController.loadPublications(2);
+                    
+                    return (all.Count != 0);
+                }
+            ));
+        }
+    }
+}
